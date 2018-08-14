68054d8b4af3888d5b59be89c7b48f9f13e0a0b0
Nikita <nik.matue@gmail.com>
2018-08-14 23:23:19 +0500

    Initial commit

---
+++ main_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.group_name
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30, blank=False, default='')
+    last_name = models.CharField(max_length=30, blank=False, default='')
+    middle_name = models.CharField(max_length=30, blank=False, default='')
+    birth_day = models.DateField(blank=True, default='')
+    stud_number = models.IntegerField(blank=False)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, default='')
+    is_captain = models.BooleanField(blank=False)
+
+    def __str__(self):
+        return '%s %s %s' % (self.last_name, self.first_name, self.middle_name)
+
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+
+
+
+
+
+
+
